9bcc6fce48fb87663a0a5402c5887acf7c249837
Varun Reddy <varunreddy0707@gmail.com>
2026-05-28 15:16:58 +0530

    Bump x86 to v0.1.6, x64 stays at v0.1.5

--- website/config.js
+++ website/config.js
@@ -1,16 +1,18 @@
 window.PHARMA_ERP_DOWNLOADS = {
-  version: "0.1.5",
+  version: "0.1.6",
   x64: {
+    version: "0.1.5",
     url: "https://github.com/varunreddy0707-stack/PharmaERP-WEB-DOWN/releases/download/v0.1.5/A2Z.Accounting.Solutions_0.1.5_x64-setup.exe",
     size: "210 MB",
     file: "A2Z Accounting Solutions_0.1.5_x64-setup.exe",
   },
   x86: {
-    url: "https://github.com/varunreddy0707-stack/PharmaERP-WEB-DOWN/releases/download/v0.1.5/A2Z.Accounting.Solutions_0.1.5_x86-setup.exe",
+    version: "0.1.6",
+    url: "https://github.com/varunreddy0707-stack/PharmaERP-WEB-DOWN/releases/download/v0.1.6/A2Z.Accounting.Solutions_0.1.6_x86-setup.exe",
     size: "180 MB",
-    file: "A2Z Accounting Solutions_0.1.5_x86-setup.exe",
+    file: "A2Z Accounting Solutions_0.1.6_x86-setup.exe",
   },
-  releaseNotesUrl: "https://github.com/varunreddy0707-stack/PharmaERP-WEB-DOWN/releases/tag/v0.1.5",
+  releaseNotesUrl: "https://github.com/varunreddy0707-stack/PharmaERP-WEB-DOWN/releases/tag/v0.1.6",
 };
 
 // deploy-refresh-2026-05-03
