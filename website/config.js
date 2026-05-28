7179541b21ed25e3fe5c1b4003d33f9cdc38e74f
Varun Reddy <varunreddy0707@gmail.com>
2026-05-28 12:59:03 +0530

    Bump to v0.1.5

--- website/config.js
+++ website/config.js
@@ -1,16 +1,16 @@
 window.PHARMA_ERP_DOWNLOADS = {
-  version: "0.1.4",
+  version: "0.1.5",
   x64: {
-    url: "https://github.com/varunreddy0707-stack/PharmaERP-WEB-DOWN/releases/download/v0.1.4/A2Z_Accounting_Solutions_0.1.4_x64-setup.exe",
-    size: "211 MB",
-    file: "A2Z_Accounting_Solutions_0.1.4_x64-setup.exe",
+    url: "https://github.com/varunreddy0707-stack/PharmaERP-WEB-DOWN/releases/download/v0.1.5/A2Z.Accounting.Solutions_0.1.5_x64-setup.exe",
+    size: "210 MB",
+    file: "A2Z Accounting Solutions_0.1.5_x64-setup.exe",
   },
   x86: {
-    url: "https://github.com/varunreddy0707-stack/PharmaERP-WEB-DOWN/releases/download/v0.1.4/A2Z_Accounting_Solutions_0.1.4_x86-setup.exe",
+    url: "https://github.com/varunreddy0707-stack/PharmaERP-WEB-DOWN/releases/download/v0.1.5/A2Z.Accounting.Solutions_0.1.5_x86-setup.exe",
     size: "180 MB",
-    file: "A2Z_Accounting_Solutions_0.1.4_x86-setup.exe",
+    file: "A2Z Accounting Solutions_0.1.5_x86-setup.exe",
   },
-  releaseNotesUrl: "https://github.com/varunreddy0707-stack/PharmaERP-WEB-DOWN/releases/tag/v0.1.4",
+  releaseNotesUrl: "https://github.com/varunreddy0707-stack/PharmaERP-WEB-DOWN/releases/tag/v0.1.5",
 };
 
 // deploy-refresh-2026-05-03
